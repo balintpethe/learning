d51145551021516de9bd258d23914a85b93ce150
Balintpethe <pethe0911@gmail.com>
2024-02-06 18:13:39 +0100

    2024.02.06-es számonkérés

---
+++ 2024.02.06/Objektum számonkérés/index.js
@@ -0,0 +1,172 @@
+const EuropaiUnio = [{
+    orszag: "Ausztria",
+    csatlakozas: "1995.01.01"
+},
+{
+    orszag: "Belgium",
+    csatlakozas: "1958.01.01"
+},
+{
+    orszag: "Bulgária",
+    csatlakozas: "2007.01.01"
+},
+{
+    orszag: "Ciprus",
+    csatlakozas: "2004.05.01"
+},
+{
+    orszag: "Csehország",
+    csatlakozas: "2004.05.01"
+},
+{
+    orszag: "Dánia",
+    csatlakozas: "1973.01.01"
+},
+{
+    orszag: "Egyesült Királyság",
+    csatlakozas: "1973.01.01"
+},
+{
+    orszag: "Észtország",
+    csatlakozas: "2004.05.01"
+},
+{
+    orszag: "Finnország",
+    csatlakozas: "1995.01.01"
+},
+{
+    orszag: "Franciaország",
+    csatlakozas: "1958.01.01"
+},
+{
+    orszag: "Görögország",
+    csatlakozas: "1981.01.01"
+},
+{
+    orszag: "Hollandia",
+    csatlakozas: "1958.01.01"
+},
+{
+    orszag: "Horvátország",
+    csatlakozas: "2013.07.01"
+},
+{
+    orszag: "Írország",
+    csatlakozas: "1973.01.01"
+},
+{
+    orszag: "Lengyelország",
+    csatlakozas: "2004.05.01"
+},
+{
+    orszag: "Lettország",
+    csatlakozas: "2004.05.01"
+},
+{
+    orszag: "Litvánia",
+    csatlakozas: "2004.05.01"
+},
+{
+    orszag: "Luxemburg",
+    csatlakozas: "1958.01.01"
+},
+{
+    orszag: "Magyarország",
+    csatlakozas: "2004.05.01"
+},
+{
+    orszag: "Málta",
+    csatlakozas: "2004.05.01"
+},
+{
+    orszag: "Németország",
+    csatlakozas: "1958.01.01"
+},
+{
+    orszag: "Olaszország",
+    csatlakozas: "1958.01.01"
+},
+{
+    orszag: "Portugália",
+    csatlakozas: "1986.01.01"
+},
+{
+    orszag: "Románia",
+    csatlakozas: "2007.01.01"
+},
+{
+    orszag: "Spanyolország",
+    csatlakozas: "1986.01.01"
+},
+{
+    orszag: "Svédország",
+    csatlakozas: "1995.01.01"
+},
+{
+    orszag: "Szlovákia",
+    csatlakozas: "2004.05.01"
+},
+{
+    orszag: "Szlovénia",
+    csatlakozas: "2004.05.01"
+}
+]
+//Hány tagja van az EU-nak?
+console.log(EuropaiUnio.length)
+//Hány ország csatlakozott 2007-ben?
+function conectionOnTime(year){
+    let darab = 0;
+    for(let i=0;i<EuropaiUnio.length;i++){
+        if(new Date(EuropaiUnio[i].csatlakozas).getFullYear() == year){
+            darab++;  
+        }
+    }
+    return darab;
+}
+console.log(conectionOnTime(2007));
+//Csatlakozott-e az adott ország
+function connectedOrNot(contry){
+    for(let i=0;i<EuropaiUnio.length;i++){
+        if(EuropaiUnio[i].orszag == contry){
+            return true;
+        }
+    }
+    return false;
+}
+console.log(connectedOrNot("Magyarország"))
+//Csatlakozás volt-e adott hónapban
+function connectionMonth(month){
+    month--
+    for(let i=0;i<EuropaiUnio.length;i++){
+        if(new Date(EuropaiUnio[i].csatlakozas).getMonth() == month){
+            return true;
+        }
+    }
+    return false;
+}
+console.log(connectionMonth(5));
+//Melyik ország csatlakozott utoljára
+function lastConnect(){
+    let max = EuropaiUnio[0];
+    for(let i=0;i<EuropaiUnio.length;i++){
+        if(new Date(EuropaiUnio[i].csatlakozas)>new Date(max.csatlakozas)){
+            max = EuropaiUnio[i]
+        }
+    }
+    return max;
+}
+console.log(lastConnect());
+//Ország statisztika
+function contryStatistic(){
+    let newAray = []
+    for(let i=0;i<EuropaiUnio.length;i++){
+        if(newAray [new Date(EuropaiUnio[i].csatlakozas).getFullYear()]){
+            newAray [new Date(EuropaiUnio[i].csatlakozas).getFullYear()]++
+        }
+        else {
+            newAray [new Date(EuropaiUnio[i].csatlakozas).getFullYear()] = 1;
+        }
+    }
+    return newAray;
+}
+console.log(contryStatistic());
